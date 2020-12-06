3749d84bf0effc7dcc9d26bd2bf5f9ff9970a474
Felix Lampe <mail@flampe.de>
2020-12-06 19:54:05 +0100

    Implement day2 part2 in bash

--- day2/countValidPasswords.sh
+++ day2/countValidPasswords.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 validPws=0
 
-while read line; do 
-    min=${line%%-*}
-    max=${line%% *}
-    max=${max##*-}
-    letter=${line%%:*}
-    letter=${letter##* }
-    password=${line##* }
+validateOldPolicy() {
     onlyPolicyCharacters=${password//[^$letter]/}
-    if [[ $min -le ${#onlyPolicyCharacters} && ${#onlyPolicyCharacters} -le $max ]]; then
+    if [[ $number1 -le ${#onlyPolicyCharacters} && ${#onlyPolicyCharacters} -le $number2 ]]; then
         (( validPws++ ))
         echo valid PW: $password with ${#onlyPolicyCharacters} ${letter} characters and input line \"$line\"
     fi
+}
+
+validateNewPolicy() {
+    char1=${password:$number1-1:1}
+    char2=${password:$number2-1:1}
+    [ "$char1" == "$letter" ]
+    char1Matches=$?
+    [ "$char2" == "$letter" ]
+    char2Matches=$?
+    if (( $char1Matches ^ $char2Matches )); then
+        (( validPws++ ))
+        echo valid PW: $password with $letter at position $([ $char1Matches -eq 0 ] && echo "$number1 but not at $number2" || echo "$number2 but not at $number1")
+    fi
+}
+
+while read line; do
+    number1=${line%%-*}
+    number2=${line%% *}
+    number2=${number2##*-}
+    letter=${line%%:*}
+    letter=${letter##* }
+    password=${line##* }
+    validateNewPolicy
 done < input
 
 echo $validPws
