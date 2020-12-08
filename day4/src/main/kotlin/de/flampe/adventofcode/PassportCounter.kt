da324fbd76767f9d0e37ea1df6f08de15629e90f
Felix Lampe <mail@flampe.de>
2020-12-08 21:16:58 +0100

    Get rid of all the control flow!

--- day4/src/main/kotlin/de/flampe/adventofcode/PassportCounter.kt
+++ day4/src/main/kotlin/de/flampe/adventofcode/PassportCounter.kt
@@ -4,21 +4,12 @@ import java.io.File
 
 val REQUIRED_FIELDS = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
-fun main(args: Array<String>) {
-    val passports = mutableListOf<String>()
-    passports.add("")
-    File("input").forEachLine {
-        if (it.isBlank() && passports.last().isNotBlank()) {
-            passports.add("")
-        } else {
-            val passportInfo = passports.removeLast() + " " + it
-            passports.add(passportInfo.trim())
-        }
-    }
+fun main() {
+    val passports = File("input").readText().split("\n\n")
     println(passports)
 
     val validPassports = passports.count { passport -> passport.isValid() }
-    println(validPassports)
+    println("Number of valid passports: $validPassports!")
 }
 
 private fun String.isValid(): Boolean {
